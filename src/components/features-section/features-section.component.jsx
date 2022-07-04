b5d8529d48db1505c3ec79981721e43b104694d5
AlexanderPolzhofer <alexander.polzhofer@gmail.com>
2022-07-04 16:41:21 +0200

    updating features component

--- src/components/features-section/features-section.component.jsx
+++ src/components/features-section/features-section.component.jsx
@@ -1,16 +1,56 @@
-import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart, faCheckCircle, faBullseye } from '@fortawesome/free-solid-svg-icons'
+import React, { useState } from 'react';
+import './features-section.styles.css';
 
+const features = [
+    {
+        id: 1,
+        title: "Easy to use",
+        description: "So easy to use, even your dog could do it.",
+        icon: faCheckCircle
+    },
+    {
+        id: 2,
+        title: 'Elite Clientele',
+        description: 'We have all the dogs, the greatest dogs.',
+        icon: faBullseye
+    },
+    {
+        id: 3,
+        title: 'Guaranteed to work.',
+        description: "Find the love of your dog's life or your money back.",
+        icon: faHeart
+    }]
 const FeaturesSection = () => {
+    const [color, setColor] = useState('#ef8172');
+
+    const handleClick = () => {
+        setColor('#ff4c68')
+    }
     return (
         <section>
-            <h3>Easy to use.</h3>
-            <p>So easy to use, even your dog could do it.</p>
-
-            <h3>Elite Clientele</h3>
-            <p>We have all the dogs, the greatest dogs.</p>
-
-            <h3>Guaranteed to work.</h3>
-            <p>Find the love of your dog's life or your money back.</p>
+            <div className='row'>
+                {features.map(feature => {
+                    const { id, title, description, icon } = feature;
+                    return (<div
+                        className='feature-box col-lg-4'
+                        key={id}>
+                        <FontAwesomeIcon
+                            onClick={handleClick}
+                            icon={icon}
+                            size='4x'
+                            style={
+                                {
+                                    color,
+                                    marginBottom: '1rem'
+                                }
+                            } />
+                        <h3>{title}</h3>
+                        <p>{description}</p>
+                    </div>)
+                })}
+            </div>
         </section>
     )
 }
